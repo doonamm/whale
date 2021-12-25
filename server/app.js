c7401bb75076c7bd318ee05eb1eb396a1e67c68c
CuCaRot <namkienlua@gmail.com>
2021-12-25 20:51:56 +0700

    third commit

--- server/app.js
+++ server/app.js
@@ -3,9 +3,11 @@ require('./app/helpers/mongo_connection');
 const PORT = process.env.PORT;
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 app.get('/', (req, res)=>{
     res.send({
@@ -29,6 +31,10 @@ app.use((err, req, res, next)=>{
             message: err.message           
         });
     }
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
 
 app.listen(PORT, ()=>console.log(`Server is running on port ${PORT}...`));
